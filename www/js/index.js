d2df4ead38a490f1ed69cff475a84cf7f653b011
Giorgos-Karachristos <73245022+Giorgos-Karachristos@users.noreply.github.com>
2022-12-20 18:04:53 +0200

    add Rules for dominoes

--- www/js/index.js
+++ www/js/index.js
@@ -2,3 +2,9 @@ function play() {
     document.getElementById("wrap1").style.display = "none"
     document.getElementById("wrap2").style.display = "block"
 }
+function openpopup() {
+    document.getElementById("popup").style.display = "block";
+}
+function closepopup() {
+    document.getElementById("popup").style.display = "none";
+}
